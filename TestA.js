0c15eeb2687d353c2238ed60c80f2c66887e240e
paraskd <104557757+paraskd@users.noreply.github.com>
2022-04-28 15:11:25 +0530

    Update TestA.js

--- TestA.js
+++ TestA.js
@@ -1,2 +1,13 @@
-import Test from './TestMJSModule.mjs';
-Test();
+const os = require('os');
+
+var osArch = os.arch();
+console.log(osArch);
+
+var osFreeMem = os.freemem();
+console.log(osFreeMem);
+
+var osUserInfo = os.userInfo();
+console.log(osUserInfo);
+
+var osTotalMem = os.totalmem();
+console.log(osTotalMem);
